2ce4df03ffd7fe5201e8b08ad7c1b6c3a8ea3816
TheDeveloperXYZ <contact@thedeveloper.xyz>
2015-12-25 18:18:09 +0200

    Indent

--- source/index.js
+++ source/index.js
@@ -5,7 +5,7 @@ const TPStylesheet = (function () {
 	const TYPE_BOOLEAN = '[object Boolean]';
 	const PREFIXES = '-webkit-,-ms-,-moz-,-o-'.split(',');
 	const PREFIXES_LEN = PREFIXES.length;
-  
+
 	class CTPStylesheet {
 		constructor (obj) {
 			this._defaults = {
@@ -18,8 +18,8 @@ const TPStylesheet = (function () {
 			this._win = obj.win;
 			this._doc = this._win.document;
 			this._styleSheetEnabled = false;
-	    this._stylesheet = this._initializeStyleElement(obj.target);
-	    this._rules = [];
+			this._stylesheet = this._initializeStyleElement(obj.target);
+			this._rules = [];
 			this._CACHED_STYLES = {};
 			this._BROWSER_STYLES= [].slice.call(this._win.getComputedStyle(this._doc.documentElement));
 		}
@@ -49,20 +49,20 @@ const TPStylesheet = (function () {
 		}
 
 		_initializeStyleElement (target) {
-      let style = this._doc.createElement('style');
-      style.type = 'text/css';
-      style.appendChild(this._doc.createTextNode(''));
+			let style = this._doc.createElement('style');
+			style.type = 'text/css';
+			style.appendChild(this._doc.createTextNode(''));
 
-      if (!target && !this._isElement(target)) {
-        this._doc.head.appendChild(style);
-      } else {
-        target.appendChild(style);
-      }
+			if (!target && !this._isElement(target)) {
+				this._doc.head.appendChild(style);
+			} else {
+				target.appendChild(style);
+			}
 
-      this._styleSheetEnabled = true;
+			this._styleSheetEnabled = true;
 
-      return style.sheet ? style.sheet : style.styleSheet;
-    }
+			return style.sheet ? style.sheet : style.styleSheet;
+		}
 
 		_dasherize (property) {
 			return property.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
@@ -70,176 +70,176 @@ const TPStylesheet = (function () {
 
 		_normalizeProperty (property) {
 			if (this._CACHED_STYLES.hasOwnProperty(property)) {
-		    return this._CACHED_STYLES[property];
-		  }
+				return this._CACHED_STYLES[property];
+			}
 
-		  let dasherizedProperty = this._dasherize(property);
+			let dasherizedProperty = this._dasherize(property);
 
 			if (this._BROWSER_STYLES.indexOf(dasherizedProperty) !== -1) {
-		    this._CACHED_STYLES[property] = dasherizedProperty;
-		    return dasherizedProperty;
-		  } else {
-		    let i = PREFIXES_LEN - 1,
-		        prefixed = null;
+				this._CACHED_STYLES[property] = dasherizedProperty;
+				return dasherizedProperty;
+			} else {
+				let i = PREFIXES_LEN - 1,
+				prefixed = null;
 
-		    while(i >= 0) {
-		      prefixed = `${PREFIXES[i]}${dasherizedProperty}`;
+				while(i >= 0) {
+					prefixed = `${PREFIXES[i]}${dasherizedProperty}`;
 
-		      if (this._BROWSER_STYLES.indexOf(prefixed) !== -1) {
-		        this._CACHED_STYLES[property] = prefixed;
-		        return prefixed;
-		      }
+					if (this._BROWSER_STYLES.indexOf(prefixed) !== -1) {
+						this._CACHED_STYLES[property] = prefixed;
+						return prefixed;
+					}
 
-		      i--;
-		    }
-		  }
+					i--;
+				}
+			}
 
 			this._CACHED_STYLES[property] = dasherizedProperty;
 
 			return dasherizedProperty;
 		}
 
-    _parseStyles (styles) {
-      if (this._isString(styles)) {
-        return styles;
-      } else if (!styles && !this._isObject(styles)) {
-        return '';
+		_parseStyles (styles) {
+			if (this._isString(styles)) {
+				return styles;
+			} else if (!styles && !this._isObject(styles)) {
+				return '';
 			}
 
 			const stylesToText = Object.keys(styles).map((key) => {
-        const property = this._normalizeProperty(key);
-        const value = styles[key];
-        const declaration = `${property}:${value}`;
+				const property = this._normalizeProperty(key);
+				const value = styles[key];
+				const declaration = `${property}:${value}`;
 
-        return declaration;
-      }).join(';');
+				return declaration;
+			}).join(';');
 
 			return `${stylesToText};`;
-    }
+		}
 
-    _insertRule (selector, styles, isImportant) {
-      const sheet = this._stylesheet;
-      const len = sheet.cssRules.length;
-      const styleRule = `${selector} { ${styles} ${isImportant ? '!important' : ''}}`;
+		_insertRule (selector, styles, isImportant) {
+			const sheet = this._stylesheet;
+			const len = sheet.cssRules.length;
+			const styleRule = `${selector} { ${styles} ${isImportant ? '!important' : ''}}`;
 
-      this._rules.push({
+			this._rules.push({
 				index: len,
-        selector: selector,
-        styles: styles,
-        isImportant: isImportant
-      });
+				selector: selector,
+				styles: styles,
+				isImportant: isImportant
+			});
 
-      if (sheet.insertRule) {
-        sheet.insertRule(styleRule, len);
-      } else if (sheet.addRule) {
-        sheet.addRule(selector, styles, len);
-      }
-    }
+			if (sheet.insertRule) {
+				sheet.insertRule(styleRule, len);
+			} else if (sheet.addRule) {
+				sheet.addRule(selector, styles, len);
+			}
+		}
 
-    _insertObjectRules (rules) {
-      for (let rule in rules) {
-        if (rules.hasOwnProperty(rule)) {
-          const selector = rule;
-          const selectorStyles = rules[rule];
-          const parsedStyle = this._parseStyles(selectorStyles);
+		_insertObjectRules (rules) {
+			for (let rule in rules) {
+				if (rules.hasOwnProperty(rule)) {
+					const selector = rule;
+					const selectorStyles = rules[rule];
+					const parsedStyle = this._parseStyles(selectorStyles);
 
-          this._insertRule(selector, parsedStyle);
-        }
-      }
-    }
+					this._insertRule(selector, parsedStyle);
+				}
+			}
+		}
 
-    _insertStringAndObjectRules (selector, rules, isImportant = false) {
-      const parsedStyle = this._parseStyles(rules);
+		_insertStringAndObjectRules (selector, rules, isImportant = false) {
+			const parsedStyle = this._parseStyles(rules);
 
-      this._insertRule(selector, parsedStyle, isImportant);
-    }
+			this._insertRule(selector, parsedStyle, isImportant);
+		}
 
-    _getCSSText () {
-      const style = this._stylesheet;
-      const { cssRules } = style;
-      const rulesLen = cssRules.length;
-      let cssText = [];
+		_getCSSText () {
+			const style = this._stylesheet;
+			const { cssRules } = style;
+			const rulesLen = cssRules.length;
+			let cssText = [];
 
-      if (rulesLen === 0) {
-        return '';
-      }
+			if (rulesLen === 0) {
+				return '';
+			}
 
-      for (let i = 0; i < rulesLen; i++) {
-        cssText.push(cssRules[i].cssText);
-      }
+			for (let i = 0; i < rulesLen; i++) {
+				cssText.push(cssRules[i].cssText);
+			}
 
-      return cssText.join('\n');
-    }
+			return cssText.join('\n');
+		}
 
-    _disableStylesheet () {
-      if (this._styleSheetEnabled) {
-        const sheet = this._stylesheet;
+		_disableStylesheet () {
+			if (this._styleSheetEnabled) {
+				const sheet = this._stylesheet;
 
-        sheet.disabled = true;
-        this._styleSheetEnabled = false;
-      }
-    }
+				sheet.disabled = true;
+				this._styleSheetEnabled = false;
+			}
+		}
 
-    _enableStylesheet () {
-      if (!this._styleSheetEnabled) {
-        const sheet = this._stylesheet;
+		_enableStylesheet () {
+			if (!this._styleSheetEnabled) {
+				const sheet = this._stylesheet;
 
-        sheet.disabled = false;
-        this._styleSheetEnabled = true;
-      }
-    }
+				sheet.disabled = false;
+				this._styleSheetEnabled = true;
+			}
+		}
 
-    add () {
-      const args = arguments;
-      const argsLen = args.length;
-      const fArg = args[0];
-      const sArg = args[1];
-      const tArg = args[2];
+		add () {
+			const args = arguments;
+			const argsLen = args.length;
+			const fArg = args[0];
+			const sArg = args[1];
+			const tArg = args[2];
 
-      switch (argsLen) {
-        case 1: {
-          if (this._isObject(fArg)) {
-            this._insertObjectRules(fArg);
-          }
+			switch (argsLen) {
+				case 1: {
+					if (this._isObject(fArg)) {
+						this._insertObjectRules(fArg);
+					}
 
-          break;
-        }
+					break;
+				}
 
-        case 2: {
-          if (this._isString(fArg) && this._isObject(sArg)) {
-            this._insertStringAndObjectRules(fArg, sArg);
-          } else if (this._isString(fArg) && this._isString(sArg)) {
+				case 2: {
+					if (this._isString(fArg) && this._isObject(sArg)) {
+						this._insertStringAndObjectRules(fArg, sArg);
+					} else if (this._isString(fArg) && this._isString(sArg)) {
 						this._insertRule(fArg, sArg);
 					}
 
 					break;
-        }
+				}
 
-        case 3: {
-          if (this._isString(fArg) && this._isObject(sArg) && this._isBoolean(tArg)) {
-            this._insertStringAndObjectRules(fArg, sArg, tArg);
-          }
+				case 3: {
+					if (this._isString(fArg) && this._isObject(sArg) && this._isBoolean(tArg)) {
+						this._insertStringAndObjectRules(fArg, sArg, tArg);
+					}
 
 					break;
-        }
-      }
+				}
+			}
 
 			return this;
-    }
+		}
 
-    disable () {
-      this._disableStylesheet();
+		disable () {
+			this._disableStylesheet();
 			return this;
-    }
+		}
 
-    enable () {
-      this._enableStylesheet();
+		enable () {
+			this._enableStylesheet();
 			return this;
-    }
+		}
 
-    CSSText () {
-      return this._getCSSText();
-    }
+		CSSText () {
+			return this._getCSSText();
+		}
 	}
 
 	return CTPStylesheet;
